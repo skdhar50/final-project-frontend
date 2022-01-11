a31d68ca36d9438a71c136c1b77d512b0b238f15
iammeskat <meskatcse@gmail.com>
2022-01-11 16:19:54 +0600

    home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			container: {
+				padding: {
+				  '2xl': '9rem',
+				},
+			  },
+		},
 	},
 	plugins: [require("@tailwindcss/forms")],
 };
